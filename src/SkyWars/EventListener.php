267550fb7f2ab2cdfa4ee3d44a9485f8023627dc
MasApip <52627239+MasApip@users.noreply.github.com>
2020-04-03 12:06:48 +0700

    Wut?

--- src/SkyWars/EventListener.php
+++ src/SkyWars/EventListener.php
@@ -104,7 +104,6 @@ class EventListener implements Listener {
             $arena->closePlayer($player);
         }
         if ($item->getCustomName() == "§r§aKit selector" && $item->getId() == 54){
-	    $player->getInventory()->setHeldItemIndex(1);
             $this->kitForm($player);
         }
         if (($block->getId() === Block::SIGN_POST || $block->getId() === Block::WALL_SIGN) && $event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
